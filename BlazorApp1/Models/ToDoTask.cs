47c5d26df17bbad1b708717b7077f407578ff259
subhav2003 <subhavosti2@gmail.com>
2024-12-19 10:48:28 +0545

    Initial expense tracker project upload

---
+++ BlazorApp1/Models/ToDoTask.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorApp1.Models
+{
+    public class TodTask
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required string Title { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool IsCompleted { get; set; }
+        public DateTime? CompletedAt { get; set; }  // Allow nulls when task is incomplete
+        public bool IsImportant { get; set; }  // Fixed the type for IsImportant
+
+        public void ToggleCompletion()
+        {
+            IsCompleted = !IsCompleted;
+            if (IsCompleted)
+            {
+                CompletedAt = DateTime.Now;  // Set completion time
+            }
+            else
+            {
+                CompletedAt = null;  // Reset when marking incomplete
+            }
+        }
+    }
+}
